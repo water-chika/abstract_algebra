9d6aa996af66aa3a77fe49103cf478ae58991e81
Water Chika <water_chika@outlook.com>
2025-06-02 17:01:34 +0000

    Add N,Z,Q,R,C number sets and belong_to

--- include/abstract_algebra.hpp
+++ include/abstract_algebra.hpp
@@ -2,8 +2,89 @@
 
 #include <type_traits>
 #include <concepts>
+#include <cmath>
+#include <complex>
 
 namespace abstract_algebra {
+
+struct natural_number {} N;
+struct integer_number {} Z;
+struct rational_number {} Q;
+struct real_number {} R;
+struct complex_number {} C;
+
+constexpr bool is_subset(natural_number N, integer_number Z) {
+    return true;
+}
+constexpr bool is_subset(integer_number Z, rational_number Q) {
+    return true;
+}
+constexpr bool is_subset(rational_number Q, real_number R) {
+    return true;
+}
+constexpr bool is_subset(real_number R, complex_number C) {
+    return true;
+}
+
+constexpr bool belong_to(std::integral auto i, natural_number N) {
+    return i >= 0;
+}
+constexpr bool belong_to(std::floating_point auto f, natural_number N) {
+    auto integer_part = f;
+    return !std::isinf(f) && f >= 0 && 0 == std::modf(f, &integer_part);
+}
+template<typename T>
+constexpr bool belong_to(std::complex<T> c, natural_number N) {
+    return std::imag(c) == 0 && belong_to(std::real(c), N);
+}
+
+constexpr bool belong_to(std::integral auto i, integer_number Z) {
+    return true;
+}
+constexpr bool belong_to(std::floating_point auto f, integer_number Z) {
+    auto integer_part = f;
+    return !std::isinf(f) && 0 == std::modf(f, &integer_part);
+}
+template<typename T>
+constexpr bool belong_to(std::complex<T> c, integer_number Z) {
+    return std::imag(c) == 0 && belong_to(std::real(c), N);
+}
+
+constexpr bool belong_to(std::integral auto i, rational_number Q) {
+    return true;
+}
+constexpr bool belong_to(std::floating_point auto f, rational_number Q) {
+    auto fp_class = std::fpclassify(f);
+    return fp_class == FP_NORMAL ||
+        fp_class == FP_SUBNORMAL ||
+        fp_class == FP_ZERO;
+}
+template<typename T>
+constexpr bool belong_to(std::complex<T> c, rational_number Q) {
+    return std::imag(c) == 0 && belong_to(std::real(c), Q);
+}
+
+constexpr bool belong_to(std::integral auto i, real_number R) {
+    return true;
+}
+constexpr bool belong_to(std::floating_point auto f, real_number R) {
+    return belong_to(f, Q);
+}
+template<typename T>
+constexpr bool belong_to(std::complex<T> c, real_number R) {
+    return std::imag(c) == 0 && belong_to(std::real(c), R);
+}
+
+constexpr bool belong_to(std::integral auto i, complex_number C) {
+    return true;
+}
+constexpr bool belong_to(std::floating_point auto f, complex_number C) {
+    return belong_to(f, R);
+}
+template<typename T>
+constexpr bool belong_to(std::complex<T> c, complex_number C) {
+    return belong_to(std::real(c), R) && belong_to(std::imag(c), R);
+}
 
 template<typename BinOp, typename G>
 concept associative = false;
--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 ﻿#include <abstract_algebra.hpp>
-#include <functional>
-
-    template<>
-    concept associative<std::plus<int>, int> = false;
-    template<>
-    concept with_identity<std::plus<int>, int> = false;
-    template<>
-    concept with_inverse<std::plus<int>, int> = false;
+#include <iostream>
 
 int main() {
-
+    float x = 7.0;
+    using namespace abstract_algebra;
+    if (belong_to(x, N)) {
+        std::cout << x << " belong to N" << std::endl;
+    }
+    std::complex<double> c = 7.0;
+    if (belong_to(c, N)) {
+        std::cout << c << " belong to N" << std::endl;
+    }
     return 0;
 }
+
